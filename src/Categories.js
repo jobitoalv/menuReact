24d79c4f0d13464b55a56789a9688e7fb91650c6
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 14:32:06 +1000

    fiing button fucntion

--- src/Categories.js
+++ src/Categories.js
@@ -3,18 +3,10 @@ import React from 'react';
 const Categories = ({ categories, filterItems }) => {
 
   return <div className='btn-container'>
-    <button className='filter-btn' onClick={()=>{filterItems('breakfast')}}>
-      breakfast
-    </button>
-    <button className='filter-btn' onClick={()=>{filterItems('lunch')}}>
-      lunch
-    </button>
-    <button className='filter-btn' onClick={()=>{filterItems('shakes')}}>
-      shakes
-    </button>
-    <button className='filter-btn' onClick={()=>{filterItems('all')}}>
-      all
-    </button>
+    {categories.map((category,index) => {
+      return <button type='button' className='filter-btn' key={index}
+      onClick ={()=> filterItems}> {category}</button>
+    })}
   </div>;
 };
 
