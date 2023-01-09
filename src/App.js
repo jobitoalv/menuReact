0fb5df794f43e30d94acd39c677586379f8ecaa0
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 13:39:02 +1000

    passing menu item to menu pros

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  const [menu, setMenu] = useState(menu);
-  
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+ 
+  return <main>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories/>
+      <Menu items={menuItems}/>
+
+    </section>
+  </main>;
 }
 
 export default App;
