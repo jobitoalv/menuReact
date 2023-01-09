de38529008f37cf2328e64856ebf12b29920f4fc
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 13:55:41 +1000

    add filter to set category

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import items from './data';
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems= (category) => {
+    const newItems = items.filter((item)=> item.category === category)
+    setMenuItems(newItems)
+  }
  
   return <main>
     <section className='menu section'>
